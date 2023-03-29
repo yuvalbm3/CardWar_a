639249a7aff47f380524a02d0a6d788fd89eb6cc
Yuval Bar Maoz <92522577+yuvalbm3@users.noreply.github.com>
2023-03-29 22:57:21 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -1,3 +1,5 @@
+//I've used chatGPT to convert output print onto string in line 68
+
 #include <iostream>
 #include <stdexcept>
 #include <sstream>
@@ -10,44 +12,87 @@ using namespace std;
 using namespace ariel;
 
 
-TEST_CASE("Setup the game") {
-    Player p1("Alice");
-    Player p2("Bob");
-
+TEST_CASE("Play the game") {
+//initial the game
+    Player p1("Kobi");
+    Player p2("Bobi");
     Game game(p1,p2);
-
+//Game start
     CHECK(p1.stacksize() == 26);
     CHECK(p2.stacksize() == 26);
+    CHECK_EQ(p1.stacksize(), p2.stacksize());
     CHECK(p1.cardesTaken() == 0);
     CHECK(p2.cardesTaken()==0);
+    CHECK_EQ(p1.cardesTaken(), p2.cardesTaken());
+//Game is running
+//play 5 turns
+    for(int i=0 ; i<5 ; i++){
+        CHECK_NOTHROW(game.playTurn());
+    }
+//Initial boolean variables
+    bool stackTest = false;
+    bool cardsTakenTest = false;
+//at least one of the player going to lose cards.
+    CHECK(p1.stacksize() <= 26);
+    CHECK(p2.stacksize() <= 26);
+    if(p1.stacksize() < 26 || p2.stacksize() < 26){
+        stackTest = true;
+    }
+    CHECK(stackTest);
+//at least one of the player going to win cards.
+    CHECK(p1.cardesTaken() >= 0);
+    CHECK(p2.cardesTaken() >= 0);
+    if(p1.cardesTaken() > 0 || p2.cardesTaken() > 0){
+        cardsTakenTest = true;
+    }
+    CHECK(cardsTakenTest);
+    CHECK_EQ(stackTest, cardsTakenTest);
+//continue play
+    game.playAll();
+//At the end of the game the sum of all cards taken must be 26
+    CHECK(p1.cardesTaken() + p2.cardesTaken() == 52);
+//At the end of the game, both player have 0 cards in their stacks
+    CHECK(p1.stacksize() == 0);
+    CHECK(p2.stacksize() == 0);
+    CHECK_EQ(p1.stacksize(), p2.stacksize());
 }
 
 TEST_CASE("Check the winner"){
-    Player p1("Alice");
-    Player p2("Bob");
+    Player p1("Kobi");
+    Player p2("Bobi");
     Game game(p1,p2);
     game.playAll();
-    //convert print to string
+//convert print to string
     std::ostringstream oss;
     game.printWiner();
     std::string output = oss.str();
+//check who won
     if (p1.getName() == output){
-        CHECK(p1.stacksize() == 52);
-        CHECK(p2.stacksize() == 0);
-        CHECK(p1.cardesTaken() == 26);
-        CHECK(p2.cardesTaken()==0);
+    CHECK(p1.stacksize() == 0);
+    CHECK(p2.stacksize() == 0);
+    CHECK_EQ(p1.stacksize(), p2.stacksize());
+    CHECK(p1.cardesTaken() > p2.cardesTaken());
+    CHECK_EQ(p1.cardesTaken() + p2.cardesTaken(), 52);
     }
     else if (p2.getName() == output){
-        CHECK(p2.stacksize() == 52);
-        CHECK(p1.stacksize() == 0);
-        CHECK(p2.cardesTaken() == 26);
-        CHECK(p1.cardesTaken()==0);
+    CHECK(p1.stacksize() == 0);
+    CHECK(p2.stacksize() == 0);
+    CHECK_EQ(p1.stacksize(), p2.stacksize());
+    CHECK(p1.cardesTaken() < p2.cardesTaken());
+    CHECK_EQ(p1.cardesTaken() + p2.cardesTaken(), 52);
     }
 };
 
-TEST_CASE("Test 3 - different players"){
-    string ali = "Alice";
-    Player p1(ali);
-    Player p2(ali);
-    CHECK(p1.getName() != p2.getName());
+TEST_CASE("THROWS exceptions"){
+//Initial game with only one player
+    Player p1("Kobi");
+    Player p2("Bobi");
+    CHECK_THROWS(Game(p1,p1));
+    
+    Game game(p1,p2);
+    game.playAll();
+//Can't play another turn cause all turn were played already
+    CHECK_THROWS(game.playTurn());
+    CHECK_THROWS(game.playAll());
 };
+
